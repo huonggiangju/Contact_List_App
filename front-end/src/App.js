aa1ad18ad8d8d1db36139c7827195403fa770fe9
servian-peter <peter.tran@servian.com>
2023-01-24 10:31:11 +1100

    Clean up code
    Just make it a self-closing tag when needed, when there are nested element, then the tag can be opened then closed.
    
    For importing using JSON format, spacing will make the code more readable.
    
    Use `const` when possible, especially when defining an exported property.
    
    Notice that trailing new line is not acceptable when the company runs linting check.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,23 @@
 
 import './App.css';
 import React from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Narbar';
 import ContactList from './components/contacts/ContactList/ContactList';
 import AddContact from './components/contacts/AddContact/AddContact';
 import ViewContact from './components/contacts/ViewContact/ViewContact';
 
-
-let App = () => {
+const App = () => {
   return (
     <React.Fragment>
-      <Navbar>
-      </Navbar>
+      <Navbar />
 
       <Routes>
-        <Route path={'/'} element={<Navigate to={'/contacts/list'}/>}></Route>
-        <Route path={'/contacts/list'} element={<ContactList/>}></Route>
-        <Route path={'/contacts/add'} element={<AddContact/>}></Route>
-        <Route path={'/contacts/view/:id'} element={<ViewContact/>}></Route>
-        <Route path={'/contacts/edit/:id'} element={<AddContact/>}></Route>
+        <Route path={'/'} element={<Navigate to={'/contacts/list'} />} />
+        <Route path={'/contacts/list'} element={<ContactList/>} />
+        <Route path={'/contacts/add'} element={<AddContact/>} />
+        <Route path={'/contacts/view/:id'} element={<ViewContact />} />
+        <Route path={'/contacts/edit/:id'} element={<AddContact />} />
       </Routes>
 
     </React.Fragment>
